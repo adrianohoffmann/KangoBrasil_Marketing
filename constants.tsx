69c46c394ec7042887cd9d8e0f3b0e6f033b258b
adrianohoffmann <113382274+adrianohoffmann@users.noreply.github.com>
2026-02-12 17:10:02 -0300

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -12,7 +12,7 @@ export const INITIAL_DATA = [
   {
     "title": "Planejamento Marketing 2026",
     "category": "Outros",
-    "coverUrl": "https://www.kango.com.br/wp-content/uploads/2026/02/Marketing.jpg",
+    "coverUrl": "https://www.kango.com.br/wp-content/uploads/2026/02/Marketing2.jpg",
     "pdfUrl": "https://drive.google.com/file/d/1lUI4KHHPlFbW-6Wxkwb-t5rrBjaH2J8o/view?usp=drive_link",
     "description": "Do conceito à execução. Dê um play e descubra os bastidores da estratégia que vai elevar o patamar da Kango Brasil no mercado de infraestrutura esportiva em 2026.",
     "isFeatured": false,
